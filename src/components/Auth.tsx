0b435b156e33177a4cd46659999ad54be5bda6a3
Tri Nguyen Van <nguyenvantri2820@gmail.com>
2024-12-08 12:14:21 +0100

    fix 12 14

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -10,7 +10,7 @@ import {
   signedOut,
 } from "~/global";
 import { ls_host } from "~/local";
-import { db } from "./database";
+import { db, Profile } from "./database";
 import LandingPage from "./LandingPage";
 import SplashScreen from "./SplashScreen";
 const names = [
@@ -40,22 +40,22 @@ async function loadGuestProfile() {
   );
   if (profile_jwt_token) {
   } else {
-    // const random_default_profile: Partial<Profile> = {
-    //   avatar_src: `${window.location.origin}/default-avatar.svg`,
-    //   banner_src: `${window.location.origin}/default-banner.jpg`,
-    //   contacts: [],
-    //   description: "Hello everyone.",
-    //   name: names[Math.floor(Math.random() * names.length)],
-    //   role: "ThirdCloud User",
-    // };
-    // console.log("call profile_create");
+    const random_default_profile: Partial<Profile> = {
+      avatar_src: `${window.location.origin}/default-avatar.svg`,
+      banner_src: `${window.location.origin}/default-banner.jpg`,
+      contacts: [],
+      description: "Hello everyone.",
+      name: names[Math.floor(Math.random() * names.length)],
+      role: "ThirdCloud User",
+    };
+    console.log("call profile_create");
     // profile_jwt_token = await profile_create(random_default_profile);
     // console.log("profile_jwt_token", profile_jwt_token);
     // ls_host.setItem("profile_jwt_token", profile_jwt_token);
   }
-  // console.log("call profile_read");
+  console.log("call profile_read");
   // const profile = await profile_read(profile_jwt_token);
-  // console.log("profile", profile);
+  console.log("profile", profile);
   // setProfileJwtToken(profile_jwt_token);
   // setProfile(profile as Profile);
   resolve_done(profile);
