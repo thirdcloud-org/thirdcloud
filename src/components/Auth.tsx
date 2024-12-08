50717a712369102c021c647e9f00627706b8f637
Tri Nguyen Van <nguyenvantri2820@gmail.com>
2024-12-08 12:12:22 +0100

    fix 1212

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -29,36 +29,36 @@ const promise_result = new Promise((resolve) => {
   resolve_done = resolve;
 });
 async function loadGuestProfile() {
-  // if (callOnce) {
-  //   const result = await promise_result;
-  //   return result;
-  // }
-  // callOnce = true;
-  // setProfile();
-  // let profile_jwt_token: string | null = await ls_host.getItem(
-  //   "profile_jwt_token"
-  // );
-  // if (profile_jwt_token) {
-  // } else {
-  //   const random_default_profile: Partial<Profile> = {
-  //     avatar_src: `${window.location.origin}/default-avatar.svg`,
-  //     banner_src: `${window.location.origin}/default-banner.jpg`,
-  //     contacts: [],
-  //     description: "Hello everyone.",
-  //     name: names[Math.floor(Math.random() * names.length)],
-  //     role: "ThirdCloud User",
-  //   };
-  //   console.log("call profile_create");
-  //   profile_jwt_token = await profile_create(random_default_profile);
-  //   console.log("profile_jwt_token", profile_jwt_token);
-  //   ls_host.setItem("profile_jwt_token", profile_jwt_token);
-  // }
+  if (callOnce) {
+    const result = await promise_result;
+    return result;
+  }
+  callOnce = true;
+  setProfile();
+  let profile_jwt_token: string | null = await ls_host.getItem(
+    "profile_jwt_token"
+  );
+  if (profile_jwt_token) {
+  } else {
+    // const random_default_profile: Partial<Profile> = {
+    //   avatar_src: `${window.location.origin}/default-avatar.svg`,
+    //   banner_src: `${window.location.origin}/default-banner.jpg`,
+    //   contacts: [],
+    //   description: "Hello everyone.",
+    //   name: names[Math.floor(Math.random() * names.length)],
+    //   role: "ThirdCloud User",
+    // };
+    // console.log("call profile_create");
+    // profile_jwt_token = await profile_create(random_default_profile);
+    // console.log("profile_jwt_token", profile_jwt_token);
+    // ls_host.setItem("profile_jwt_token", profile_jwt_token);
+  }
   // console.log("call profile_read");
   // const profile = await profile_read(profile_jwt_token);
   // console.log("profile", profile);
   // setProfileJwtToken(profile_jwt_token);
   // setProfile(profile as Profile);
-  // resolve_done(profile);
+  resolve_done(profile);
 }
 
 export function Auth(props: { children?: JSX.Element }) {
